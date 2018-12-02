49fd55248c140f9dd2dc27df62687b207b1284d4
daixiaohan <jasonxiaohan198@139.com>
2018-12-02 22:19:11 +0800

    动态数组（数组增加、删除、查找）

--- src/Array/Array.java
+++ src/Array/Array.java
@@ -1,12 +1,12 @@
 package Array;
 
-public class Array {
-    private int[] data;
+public class Array<E> {
+    private E[] data;
     private int size;
 
     // 构造函数，传入数组的容量capacity构造Array
     public Array(int capacity) {
-        data = new int[10];
+        data = (E[]) new Object[capacity];
         size = 0;
     }
 
@@ -31,27 +31,124 @@ public class Array {
     }
 
     // 向所有元素后添加一个元素
-    public void addLast(int e) {
-       add(size, e);
+    public void addLast(E e) {
+        add(size, e);
     }
 
     // 向所有元素前添加一个新元素
-    public void addFirst(int e) {
+    public void addFirst(E e) {
         add(0, e);
     }
 
-    public void add(int index, int e) {
-        if (size == data.length) {
-            throw new IllegalArgumentException("AddLast failed.Array is full.");
-        }
-
+    public void add(int index, E e) {
         if (index < 0 || index > size) {
             throw new IllegalArgumentException("Add failed. Require index >= 0 and index <= size.");
+        }
+
+        if (size == data.length) {
+//            throw new IllegalArgumentException("AddLast failed.Array is full.");
+            resize(2 * data.length);
         }
         for (int i = size - 1; i >= index; i--) {
             data[i + 1] = data[i];
         }
         data[index] = e;
         size++;
+    }
+
+    // 获取index索引位置的元素
+    public E get(int index) {
+        if (index < 0 || index >= size) {
+            throw new IllegalArgumentException("Get failed.Index is illegal.");
+        }
+        return data[index];
+    }
+
+    // 修改index位置的索引值为e
+    public void set(int index, E e) {
+        if (index < 0 || index >= size) {
+            throw new IllegalArgumentException("Set failed.Index is illegal.");
+        }
+        data[size] = e;
+    }
+
+    // 查找数组中是否有元素e
+    public boolean contains(E e) {
+        for (int i = 0; i < size; i++) {
+            if (data[i].equals(e)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // 查找数组中元素e所在的索引，如果不存在元素e，则返回-1
+    public int find(int e) {
+        for (int i = 0; i < size; i++) {
+            if (data[i].equals(e)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    // 删除数组中指定index索引位置的元素
+    public E remove(int index) {
+        if (index < 0 || index >= size) {
+            throw new IllegalArgumentException("Remove failed.Index is illegal.");
+        }
+        E ret = data[index];
+        for (int i = index + 1; i < size; i++) {
+            data[i - 1] = data[i];
+        }
+        size--;
+        data[size] = null;
+        if (size == data.length / 4 && data.length / 2 != 0) {
+            resize(data.length / 2);
+        }
+        return ret;
+    }
+
+    // 从数组中删除第一个元素，返回删除的元素
+    public E removeFirst() {
+        return remove(0);
+    }
+
+    // 从数组中删除最后一个元素，返回删除的元素
+    public E removeLast() {
+        return remove(size - 1);
+    }
+
+    // 从数组总删除元素e
+    public void removeElement(int e) {
+        int index = find(e);
+        if (index != -1) {
+            remove(index);
+        }
+    }
+
+    @Override
+    public String toString() {
+        StringBuffer res = new StringBuffer();
+        res.append(String.format("Array: size = %d,capacity = %d\r\n", size, data.length));
+        res.append("[");
+        for (int i = 0; i < size; i++) {
+            if (i == size - 1) {
+                res.append(data[i]);
+            } else {
+                res.append(data[i] + ", ");
+            }
+        }
+        res.append("]");
+        return res.toString();
+    }
+
+    // 动态扩容新数组为旧数组的两倍
+    private void resize(int newCapacity) {
+        E[] newData = (E[]) new Object[newCapacity];
+        for (int i = 0; i < size; i++) {
+            newData[i] = data[i];
+        }
+        data = newData;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,26 @@
-public class Main {
+import Array.Array;
 
+public class Main {
     public static void main(String[] args) {
-	// write your code here
+        Array<Integer> arr = new Array();   // int类型的包装类
+        for (int i = 0; i < 10; i++) {
+           arr.addLast(i);
+        }
+        System.out.println(arr);
+
+        arr.add(1, 100);
+        System.out.println(arr);
+
+        arr.addFirst(-1);
+        System.out.println(arr);
+
+        arr.remove(2);
+        System.out.println(arr);
+
+        arr.removeElement(4);
+        System.out.println(arr);
+
+        arr.removeFirst();
+        System.out.println(arr);
     }
 }
